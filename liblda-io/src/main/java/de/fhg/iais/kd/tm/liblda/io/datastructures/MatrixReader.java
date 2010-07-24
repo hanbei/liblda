0dc6d5480f37320fca31b696c80aff6a12aa5aaa
hanbei <hanbei@4bf7f4da-c367-485a-8e4d-46ba476c993d>
2010-07-24 16:37:56 +0000

    MatrixReader stub
    git-svn-id: https://subversion.assembla.com/svn/liblda/trunk@16 4bf7f4da-c367-485a-8e4d-46ba476c993d

---
+++ liblda-io/src/main/java/de/fhg/iais/kd/tm/liblda/io/datastructures/MatrixReader.java
@@ -0,0 +1,23 @@
+package de.fhg.iais.kd.tm.liblda.io.datastructures;
+
+import de.fhg.iais.kd.tm.liblda.datastructures.Matrix;
+import de.fhg.iais.kd.tm.liblda.io.IReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+/**
+ */
+public class MatrixReader implements IReader<Matrix> {
+
+    @Override
+    public Matrix read(InputStream in) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Matrix read(Reader reader) throws IOException {
+        return null;
+    }
+}
